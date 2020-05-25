d5e800dd72e954dff84a54b1db355690d4b3a14a
guumee <guzelya93@gmail.com>
2020-05-25 11:48:06 +0300

    Added the url mapping

--- UniShed/login_user/urls.py
+++ UniShed/login_user/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 #TEMPLATE URLS!
 app_name = 'login_user'
 
 urlpatterns = [
-    url(r'^register/$', views.register, name='register')
+    path('register/', views.register, name='register')
 ]
 
