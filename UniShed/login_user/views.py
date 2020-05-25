e7984c518b483b8f3001afef45d4e13b1f146b32
guumee <guzelya93@gmail.com>
2020-05-25 14:45:30 +0300

    Added user_login and user_logout and special functions for the user

--- UniShed/login_user/views.py
+++ UniShed/login_user/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from .forms import UserForm, UserProfileInfoForm
 
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def special(request):
+    return HttpResponseRedirect("You're logged in, Nice!")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
 
 def register(request):
 
@@ -35,3 +52,28 @@ def register(request):
                             {'user_form': user_form,
                              'profile_form': profile_form,
                              'registered': registered})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse("АККАУНТ НЕ АКТИВЕН!")
+
+        else:
+            print("Кто-то пытался войти в систему, но не удалось!")
+            print("Username: {} and password: {}".format(username, password))
+            return HttpResponse("Предоставлены неверные данные для входа")
+
+    else:
+        return render(request, 'login_user/login.html')
