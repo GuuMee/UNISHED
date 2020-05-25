c5c148c0cc298ab0536fb7a127ae7bbcd1f2a805
guumee <guzelya93@gmail.com>
2020-05-25 11:44:39 +0300

    Added DatePicker Input and others for registration Form

--- UniShed/login_user/forms.py
+++ UniShed/login_user/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.conf import settings
 from django.contrib.auth.models import User
 from .models import UserProfileInfo
 from phonenumber_field.formfields import PhoneNumberField
-from django.contrib.admin.widgets import AdminDateWidget
+from bootstrap_datepicker_plus import DatePickerInput
 from django.utils.translation import gettext_lazy as _
 
 
@@ -16,7 +17,16 @@ class UserForm(forms.ModelForm):
 
 class UserProfileInfoForm(forms.ModelForm):
     phone = PhoneNumberField(label=_("Телефон"))
-    birth_date = forms.DateField(widget=AdminDateWidget(), label=_("Дата рождения"))
+    birth_date = forms.DateField(
+        widget=DatePickerInput(
+            options={'locale': settings.LANGUAGE_CODE,
+                     'minDate': settings.START_DATE,
+                     'maxDate': settings.END_DATE,
+                     },
+            format=settings.DATE_INPUT_FORMATS,
+        ),
+        label=_("Дата рождения")
+    )
 
     class Meta():
         model = UserProfileInfo
