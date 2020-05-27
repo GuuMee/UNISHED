55bd27e90bcc8ca364962fc14bd5ce89d94443eb
guumee <guzelya93@gmail.com>
2020-05-27 16:53:40 +0300

    Added some models for university strcuture

--- UniShed/univer_structure/models.py
+++ UniShed/univer_structure/models.py
@@ -99,3 +99,63 @@ class Week(models.Model):
                                      choices=WeekOfStudy.choices,
                                      default=WeekOfStudy.ODD_WEEK
                                      )
+
+class DayStudy(models.Model):
+    class DayOfStudy(models.TextChoices):
+        MONDAY = 'Понедельник', _('Пн')
+        TUESDAY = 'Вторник', _('Вт')
+        WEDNESDAY = 'Среда', _('Ср')
+        THURSDAY = 'Четверг', _('Чт')
+        FRIDAY = 'Пятница', _('Пт')
+        SATURDAY = 'Суббота', _('Сб')
+
+    week_of_study = models.CharField(max_length=20,
+                                     choices=WeekOfStudy.choices,
+                                     default=WeekOfStudy.ODD_WEEK
+                                     )
+
+class Times(models.Model):
+
+    class TimesOfStudy(models.TextChoices):
+        FIRST_CLASS_TIME = '8:00-9:30', _('1.ПАРА 8:00-9:30')
+        SECOND_CLASS_TIME = '9:40-11:10', _('2.ПАРА 9:40-11:10')
+        THIRD_CLASS_TIME = '11:20-12:50', _('3.ПАРА 11:20-12:50')
+        FOURTH_CLASS_TIME = '13:00-14:30', _('4.ПАРА 13:00-14:30')
+        FIFTH_CLASS_TIME = '14:40-16:10', _('5.ПАРА 14:40-16:10')
+        SIXTH_CLASS_TIME = '16:20-17:50', _('6.ПАРА 16:20-17:50')
+        SEVENTH_CLASS_TIME = '18:00-19:30', _('7.ПАРА 18:00-19:30')
+        EIGHTH_CLASS_TIME = '19:40-21:10', _('8.ПАРА 19:40-21:10')
+
+    times_of_study = models.CharField(max_length=30,
+                                      choices=TimesOfStudy.choices )
+
+#Specific models about students
+
+class CourseNumber(models.Model):
+
+    class CoursesOfStudent(models.TextChoices):
+        UNDER_COURSE = '0 курс', _('Подготовительный курс')
+        FIRST_COURSE = '1 курс', _('Первый курс')
+        SECOND_COURSE = '2 курс', _('Второй курс')
+        THIRD_COURSE = '3 курс', _('Третий курс')
+        FOURTH_COURSE = '4 курс', _('Четвертый курс')
+        FIFTH_COURSE = '5 курс', _('Пятый курс')
+
+    week_of_study = models.CharField(max_length=10,
+                                     choices=CoursesOfStudent.choices,
+                                     default=CoursesOfStudent.FIRST_COURSE
+                                     )
+
+
+class Degree(models.Model):
+
+    class DegreesOfStudent(models.TextChoices):
+        BACHELOR_DEGREE = 'Бакалавриат', _('Бакалавриат')
+        SPECIALIST_DEGREE = 'Специалитет', _('Специалитет')
+        MASTERS_DEGREE = 'Магистратура', _('Магистратура')
+        PHD_DEGREE = 'Аспирантура', _('Аспирантура')
+
+    week_of_study = models.CharField(max_length=10,
+                                     choices=DegreesOfStudent.choices,
+                                     default=DegreesOfStudent.FIRST_COURSE
+                                     )
