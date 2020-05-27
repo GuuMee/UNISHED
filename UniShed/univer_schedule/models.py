60f4b3dd11a707246f12ce1a7cf236f9855062e3
guumee <guzelya93@gmail.com>
2020-05-27 17:11:31 +0300

    Added some changes for models of university schedule app

--- UniShed/univer_schedule/models.py
+++ UniShed/univer_schedule/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class CourseStudent(models.Model):
-    degree_name = models.ForeignKey("univer_structure.Degree", on_delete=models.CASCADE, related_name='degrees')
-    course_number = models.ForeignKey("univer_structure.CourseNumber", on_delete=models.CASCADE, related_name="coursesnum")
-    group_number = models.ForeignKey("univer_structure.Group", on_delete=models.CASCADE, related_name="groups")
+    degree_name = models.ForeignKey("univer_structure.Degree", on_delete=models.CASCADE, related_name='+')
+    course_number = models.ForeignKey("univer_structure.CourseNumber", on_delete=models.CASCADE, related_name="+")
+    group_number = models.ForeignKey("univer_structure.Group", on_delete=models.CASCADE, related_name="+")
 
 
 class LectorAttachedDiscipline(models.Model):
@@ -15,15 +15,15 @@ class LectorAttachedDiscipline(models.Model):
 class CourseScheduleItem(models.Model):
     auditorium_of_course = models.ForeignKey("univer_structure.Auditorium", on_delete=models.CASCADE,
                                              related_name='courseauditorium')
-    lector_of_course = models.ForeignKey("univer_structure.Lector", on_delete=models.CASCADE, related_name='courselectors')
-    discipline_of_course = models.ForeignKey("univer_structure.Discipline", on_delete=models.CASCADE, related_name='disciplines')
-    student_course = models.ForeignKey(CourseStudent, on_delete=models.CASCADE, related_name='groups')
+    lector_of_course = models.ForeignKey("univer_structure.Lector", on_delete=models.CASCADE, related_name='+')
+    discipline_of_course = models.ForeignKey("univer_structure.Discipline", on_delete=models.CASCADE, related_name='+')
+    student_course = models.ForeignKey(CourseStudent, on_delete=models.CASCADE, related_name='+')
 
 
 class WeekCourseSchedule(models.Model):
-    week_of_course = models.ForeignKey("univer_structure.Week", on_delete=models.CASCADE, related_name='courseweek')
-    time_of_course = models.ForeignKey("univer_structure.Times", on_delete=models.CASCADE, related_name='coursetimes')
-    course_item = models.ForeignKey (CourseScheduleItem, on_delete=models.CASCADE, related_name='courses')
+    week_of_course = models.ForeignKey("univer_structure.Week", on_delete=models.CASCADE, related_name='+')
+    time_of_course = models.ForeignKey("univer_structure.Times", on_delete=models.CASCADE, related_name='+')
+    course_item = models.ForeignKey (CourseScheduleItem, on_delete=models.CASCADE, related_name='+')
 
 
 
